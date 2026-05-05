dc55a74a0dc3348e3cefa496c5397299b060d020
Prohovoch <w1ndoff@yandex.ru>
2026-05-05 20:05:40 +0300

    User Dto

---
+++ DTO/Users/UserPatchUpdate.cs
@@ -0,0 +1,12 @@
+﻿namespace IoT.DTO.Users
+{
+    public class UserPatchUpdate
+    {
+        // using nullable fields for that reason.
+
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
---
+++ DTO/Users/UserUpdate.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace IoT.DTO.Users
+{
+    public class UserUpdate
+    {
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "Entered more than 50 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Entered more than 50 characters")]
+        public string Surname {get; set;} = string.Empty;
+
+        [Range(0,100, ErrorMessage = "Bad value.")]
+        public int? Age { get; set;}
+    }
+}
