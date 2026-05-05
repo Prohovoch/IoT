9320134e0c107140d3ad3ecd1960087ac7555deb
Prohovoch <w1ndoff@yandex.ru>
2026-05-05 20:30:41 +0300

    Hubs dto but without patch variant

---
+++ DTO/Hubs/HubsRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IoT.DTO.Hubs
+{
+    public class HubsRequest
+    {
+        public Guid UserId;
+        
+        [MaxLength(50, ErrorMessage ="More than 50 chars")]
+        public string? HubAlias { get; set;  }
+
+        public bool? IsActive {get; set; }
+    }
+}
---
+++ DTO/Hubs/HubsResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace IoT.DTO.Hubs
+{
+    public class HubsResponse
+    {
+        public Guid Id { get; init; }
+        public string? HubAlias { get; init; }
+
+        public bool? IsActive { get; init; }
+        
+        public Guid UserId { get; init; }
+    }
+
+    public class HubsResponseExtra : HubsResponse
+    {
+        // ICollection<...>  Devices { get; init } = new List<Hub>();
+        // ICollection<...>  Robots {get; init} = new List<Robots>()
+    }
+}
